b3bfe77ba28905e934ebd46fb4bb4a77b950223e
Ellen Wendelin <ellenwendelin@live.se>
2018-11-15 15:32:43 +0800

    updated

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,35 +1,33 @@
 class DosesController < ApplicationController
-  before_action find_dose
-  def index
-
-  end
-
-  def show
-
-  end
 
   def new
-
+    @dose = Dose.new
   end
 
   def create
-
+    @dose = Dose.new(dose_params)
+    @dose.cocktails = Cocktail.find(params[:cocktail_id])
+    @dose.ingredients = Ingredient.find(params[:ingredient_id])
+    if @dose.save!
+      redirect_to new_cocktail_dose_path(@dose)
+    else
+      @dose = Dose.new(dose_params)
+      render :new
+    end
   end
 
-
   def destroy
-
+    @dose = Dose.find(params[:id])
+    @dose.destroy
   end
 
   private
 
-  def doses_params
-    params.require(:dose).permit(:description)
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
   end
 
   def find_dose
     @dose = Dose.find(params[:id])
   end
-
 end
-
